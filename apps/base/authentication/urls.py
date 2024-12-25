7befeeb897f58b8288a6a1f604dce9fa263a6547
vlad-terehoff <tereh_off95@mail.ru>
2024-12-26 02:44:53 +0500

    .

--- apps/base/authentication/urls.py
+++ apps/base/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, login
+from .views import home, login, check_status_login, success
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', login, name='login'),
+    path('check_status_login/', check_status_login, name='check_status_login'),
+    path('success/', success, name='success'),
 ]
--- apps/base/authentication/views.py
+++ apps/base/authentication/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
-from django.shortcuts import redirect
 from rest_framework.permissions import AllowAny
 from django.conf import settings
+from django.http import JsonResponse
 import uuid
+from user.models import User
 
 
 def home(request):
@@ -15,4 +16,28 @@ def home(request):
 def login(request):
     random_uuid = uuid.uuid4()
     url = f'{settings.URL_BOT}?start={random_uuid}'
-    return redirect(url)
+    return JsonResponse({'url': url, 'auth_key': str(random_uuid)})
+
+
+def success(request):
+    data = request.GET.get("name")
+    name = {'name': data}
+    return render(request, 'main/success.html', context=name)
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def check_status_login(request):
+    # token = request.data.get('token')
+    token = 'ee02947e-d879-4ea1-8078-d033b21ba8f5'
+    user: User = User.objects.filter(auth_token=token).first()
+
+    if user:
+        name = user.first_name
+        return JsonResponse({'status': 'auth', 'name': name})
+
+    return JsonResponse({'status': 'not_auth'})
+
+
+
+
